68307dc9c276f52f2934ada97df6aa2443e82a8e
SIGNINE <shamil.ishraq@gmail.com>
2014-02-08 17:43:27 -0500

    team table

--- nba/schema/team_table.sql
+++ nba/schema/team_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE team 
 (
-	id SERIAL NOT NULL PRIMARY KEY,
+  id SERIAL NOT NULL PRIMARY KEY,
   city varchar(100) NOT NULL,
   name varchar(100) NOT NULL,
   abbreviation varchar(20) NOT NULL,
