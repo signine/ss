b29a2a9f8cf0cb91bc82e40e5043757a296efeef
SIGNINE <shamil.ishraq@gmail.com>
2014-02-09 10:13:43 -0500

    first ingestion of team

--- nba/team.rb
+++ nba/team.rb
@@ -1,9 +1,30 @@
-require File.join(File.dirname(__FILE__), "clients/nba_stats")
+require_relative 'clients/nba_stats'
+require_relative 'util/db_manager'
+require 'sequel'
+
+DB = NBA::DBManager.create_connection
+
+team_table = DB[:team]
 
 c = NBAStats.new "Season" => "2013-14", "LeagueID" => "00", "SeasonType" => "Regular Season"
 teams = c.get_stat("commonteamyears")
 teams.select!{|team| team.abbreviation != nil }
 
 teams.each do |team|
-	pp c.get_stat("teaminfocommon", {"TeamID" => team.team_id})
+	info = c.get_stat("teaminfocommon", {"TeamID" => team.team_id}).first
+
+	puts "Inserting #{info.team_name}"
+	pp info
+	pp team
+
+	team_table.insert(
+	{
+		:city => info.team_city, 
+		:name => info.team_name, 
+		:abbreviation => info.team_abbreviation, 
+		:code => info.team_code, 
+		:min_year => team.min_year,
+		:max_year => team.max_year,
+		:nba_stats_id => team.team_id
+	})
 end
---
+++ nba/util/db_manager.rb
@@ -0,0 +1,14 @@
+require_relative "../nba"
+require 'sequel'
+
+module NBA
+	module DBManager
+
+		DEFAULT = {:adapter=>'postgres', :database=>'nba'}
+
+		def self.create_connection opts = {}
+			Sequel.connect(DEFAULT.merge(opts))
+		end
+
+	end
+end
