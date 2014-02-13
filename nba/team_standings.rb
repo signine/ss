b43cfa8cf8baa46e56620cddd52d6d956fb82630
SIGNINE <shamil.ishraq@gmail.com>
2014-02-12 22:44:48 -0500

    team_standings_timeline collector

--- nba/team_standings.rb
+++ nba/team_standings.rb
@@ -4,30 +4,67 @@ require 'sequel'
 require 'nokogiri'
 require 'open-uri'
 
+wins = -> (text) { text.split("-").first }
+losses = -> (text) { text.split("-").last }
+
+DEFAULTS = {:season_year => "2013-14", :season_type => "regular"}
 DB = NBA::DBManager.create_connection
-$teams = DB[:team]
-# teams.each do |team|
-#   puts team[:code]
-# end
 
-# standings = DB[:team_standings_timeline]
+TABLE = :team_standings_timeline
+COLUMNS = [:season_year, :season_type, :team, :team_conference, :team_division, :conf_rank, :div_rank, :pct, :gb, :conf_wins, :conf_losses, :div_wins, :div_losses, :home_wins, :home_losses, :road_wins, :road_losses, :wins, :losses, :total_games, :last_10_wins, :last_10_losses, :streak]
 
+loader =
+Proc.new do
+	teams = DB[:team].all
 
-#c = NBAStats.new "Season" => "2013-14", "LeagueID" => "00", "SeasonType" => "Regular Season"a
-def get_code team_name
-  $teams.select(:code).where(:city => team_name).first
+	c = NBAStats.new "Season" => "2013-14", "LeagueID" => "00", "SeasonType" => "Regular Season"
+	conf_data = []
+	teams.each {|t| conf_data << c.get_stat("teaminfocommon", "TeamID" => t[:nba_stats_id]).first}
+
+	page = Nokogiri::HTML(open("http://www.nba.com/standings/team_record_comparison/conferenceNew_Std_Alp.html"))
+	p = page.css('div#nbaFullContent table.genStatTable tr')
+	rows = p.css(".odd").to_a.concat p.css(".even").to_a
+
+	data = []
+
+	rows.each do |r|
+	  team = {}.merge DEFAULTS
+	  columns = r.css('td')
+	  code = columns[0].css("a")[0]["href"].gsub(/\//, '')
+	  info = conf_data.select {|t| t.team_code == code}.first
+
+	  team[:team] = code
+	  team[:team_conference] = info.team_conference
+	  team[:team_division] = info.team_division
+	  team[:conf_rank] = info.conf_rank
+	  team[:div_rank] = info.div_rank
+	  team[:pct] = columns[3].text.to_f
+	  team[:gb] = columns[4].text.to_f
+	  team[:conf_wins] = wins.call columns[5].text
+	  team[:conf_losses] = losses.call columns[5].text
+	  team[:div_wins] = wins.call columns[6].text
+	  team[:div_losses] = losses.call columns[6].text
+	  team[:home_wins] = wins.call columns[7].text
+	  team[:home_losses] = losses.call columns[7].text
+	  team[:road_wins] = wins.call columns[8].text
+	  team[:road_losses] = losses.call columns[8].text
+	  team[:wins] = info.w
+	  team[:losses] = info.l
+	  team[:total_games] = info.w = info.l
+	  team[:last_10_wins] = wins.call columns[9].text
+	  team[:last_10_losses] = losses.call columns[9].text
+	  team[:streak] = columns[10].text
+
+	  data << team
+	end
+	data
 end
 
-page = Nokogiri::HTML(open("http://www.nba.com/standings/team_record_comparison/conferenceNew_Std_Alp.html"))
-p = page.css('div#nbaFullContent table.genStatTable tr')
-rows = p.css(".odd").to_a.concat p.css(".even").to_a
+collector = NBA::DataCollector.new loader, TABLE, COLUMNS
 
-rows.each do |r|
-  team = {} 
-  coulms = r.css('td')
-  code = coulms[0].css("a")[0]["href"].gsub(/\//, '')
-  p code
-end
+collector.run ARGV
+
+
 
 
 
